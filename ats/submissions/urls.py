bdd063cf362d3b7a664cef594053e65195044c0a
keitaflora01 <keitafloraprincesses@gmail.com>
2026-02-04 06:08:02 +0100

    d

--- ats/submissions/urls.py
+++ ats/submissions/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ats.submissions.api.views.submission_views import SubmissionDetailView, SubmissionCreateView, SubmissionListView
 
 urlpatterns = [
-    path('api/submissions/', SubmissionListView.as_view(), name='submission-list'),          # ← GET sur cette URL
-    path('api/submissions/create/', SubmissionCreateView.as_view(), name='submission-create'),  # ← POST ici (ou sur le même)
+    path('api/submissions/', SubmissionListView.as_view(), name='submission-list'),         
+    path('api/submissions/create/', SubmissionCreateView.as_view(), name='submission-create'),  
     path('api/submissions/<uuid:id>/', SubmissionDetailView.as_view(), name='submission-detail'),
 ]
