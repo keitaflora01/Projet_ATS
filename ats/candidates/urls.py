5a2c32799b1c71788bdf884e80f29fb1b444276a
keitaflora01 <keitafloraprincesses@gmail.com>
2026-02-04 08:51:42 +0100

    d

--- ats/candidates/urls.py
+++ ats/candidates/urls.py
@@ -4,5 +4,5 @@ from ats.candidates.api.views.candidates_views import CandidateDetailView, Candi
 
 urlpatterns = [
     path("api/candidates/", CandidateListView.as_view(), name="candidate-list"),
-    path("api/candidates/<uuid:pk>/", CandidateDetailView.as_view(), name="candidate-detail"),
+    path("api/candidates/<uuid:id>/", CandidateDetailView.as_view(), name="candidate-detail"),
 ]
--- ats/recruiters/urls.py
+++ ats/recruiters/urls.py
@@ -3,5 +3,5 @@ from .api.views.recruiters_views import RecruiterListView, RecruiterDetailView
 
 urlpatterns = [
     path("api/recruiters/", RecruiterListView.as_view(), name="recruiter-list"),
-    path("api/recruiters/<uuid:pk>/", RecruiterDetailView.as_view(), name="recruiter-detail"),
+    path("api/recruiters/<uuid:id>/", RecruiterDetailView.as_view(), name="recruiter-detail"),
 ]
